203ccc83710dad323d10c90079e5a0ebb92f0c75
Larisa Skrashchuk <lskrashchuk@gmail.com>
2016-08-16 18:20:30 +0300

    Implemented TagDao and UrlDao classes

--- dataaccess/src/main/java/by/lskrashchuk/test/shrtly/dataaccess/impl/TagDaoImpl.java
+++ dataaccess/src/main/java/by/lskrashchuk/test/shrtly/dataaccess/impl/TagDaoImpl.java
@@ -2,11 +2,23 @@ package by.lskrashchuk.test.shrtly.dataaccess.impl;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.jpa.criteria.OrderImpl;
 import org.springframework.stereotype.Repository;
 
 import by.lskrashchuk.test.shrtly.dataaccess.TagDao;
 import by.lskrashchuk.test.shrtly.dataaccess.filters.TagFilter;
 import by.lskrashchuk.test.shrtly.datamodel.Tag;
+import by.lskrashchuk.test.shrtly.datamodel.Tag_;
+import by.lskrashchuk.test.shrtly.datamodel.UserProfile;
+import by.lskrashchuk.test.shrtly.datamodel.UserProfile_;
 
 @Repository
 public class TagDaoImpl extends AbstractDaoImpl<Tag, Long> implements TagDao{
@@ -17,26 +29,103 @@ public class TagDaoImpl extends AbstractDaoImpl<Tag, Long> implements TagDao{
 
 	@Override
 	public Long count(TagFilter filter) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = getEntityManager();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+		Root<Tag> from = cq.from(Tag.class);
+		cq.select(cb.count(from));
+		TypedQuery<Long> q = em.createQuery(cq);
+		return q.getSingleResult();
 	}
 
 	@Override
 	public List<Tag> find(TagFilter filter) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = getEntityManager();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+
+		CriteriaQuery<Tag> cq = cb.createQuery(Tag.class);
+
+		Root<Tag> from = cq.from(Tag.class);
+
+		// set selection
+		cq.select(from);
+
+		if (filter.getName() != null) {
+			Predicate fNameEqualCondition = cb.equal(from.get(Tag_.name), filter.getName());
+			cq.where(fNameEqualCondition);
+		}
+
+		// set sort params
+		if (filter.getSortProperty() != null) {
+			cq.orderBy(new OrderImpl(from.get(filter.getSortProperty()), filter.isSortOrder()));
+		}
+
+		TypedQuery<Tag> q = em.createQuery(cq);
+
+		// set paging
+		if (filter.getOffset() != null && filter.getLimit() != null) {
+			q.setFirstResult(filter.getOffset());
+			q.setMaxResults(filter.getLimit());
+		}
+
+		// set execute query
+		List<Tag> allitems = q.getResultList();
+		return allitems;
 	}
 
 	@Override
 	public Tag find(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = getEntityManager();
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+
+		CriteriaQuery<Tag> cq = cb.createQuery(Tag.class);
+
+		Root<Tag> from = cq.from(Tag.class);
+
+		cq.select(from);
+		Predicate nameEqualCondition = cb.equal(from.get(Tag_.name), name);
+		cq.where(nameEqualCondition);
+
+		TypedQuery<Tag> q = em.createQuery(cq);
+
+		List<Tag> allitems = q.getResultList();
+
+		if (allitems.isEmpty()) {
+			return null;
+		} else if (allitems.size() == 1) {
+			return allitems.get(0);
+		} else {
+			throw new IllegalArgumentException("more than 1 user found ");
+		}
 	}
 
 	@Override
 	public Tag getWithUrls(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = getEntityManager();
+
+		if (em.find(getEntityClass(), id) == null) {
+			return null;
+		};
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+
+		CriteriaQuery<Tag> cq = cb.createQuery(Tag.class);
+
+		Root<Tag> from = cq.from(Tag.class);
+
+		// set selection
+		cq.select(from);
+
+		from.fetch(Tag_.urls, JoinType.LEFT);
+
+		cq.where(cb.equal(from.get(Tag_.id), id));
+		cq.distinct(true);
+
+		TypedQuery<Tag> q = em.createQuery(cq);
+
+		// set execute query
+		return q.getSingleResult();
 	}
 
 }
--- dataaccess/src/main/java/by/lskrashchuk/test/shrtly/dataaccess/impl/UrlDaoImpl.java
+++ dataaccess/src/main/java/by/lskrashchuk/test/shrtly/dataaccess/impl/UrlDaoImpl.java
@@ -2,11 +2,23 @@ package by.lskrashchuk.test.shrtly.dataaccess.impl;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.jpa.criteria.OrderImpl;
 import org.springframework.stereotype.Repository;
 
 import by.lskrashchuk.test.shrtly.dataaccess.UrlDao;
 import by.lskrashchuk.test.shrtly.dataaccess.filters.UrlFilter;
 import by.lskrashchuk.test.shrtly.datamodel.Url;
+import by.lskrashchuk.test.shrtly.datamodel.Url_;
+import by.lskrashchuk.test.shrtly.datamodel.UserProfile;
+import by.lskrashchuk.test.shrtly.datamodel.UserProfile_;
 
 @Repository
 public class UrlDaoImpl extends AbstractDaoImpl<Url, Long> implements UrlDao{
@@ -17,26 +29,103 @@ public class UrlDaoImpl extends AbstractDaoImpl<Url, Long> implements UrlDao{
 
 	@Override
 	public Long count(UrlFilter filter) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = getEntityManager();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+		Root<Url> from = cq.from(Url.class);
+		cq.select(cb.count(from));
+		TypedQuery<Long> q = em.createQuery(cq);
+		return q.getSingleResult();
 	}
 
 	@Override
 	public List<Url> find(UrlFilter filter) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = getEntityManager();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+
+		CriteriaQuery<Url> cq = cb.createQuery(Url.class);
+
+		Root<Url> from = cq.from(Url.class);
+
+		// set selection
+		cq.select(from);
+
+		if (filter.getUrlCode() != null) {
+			Predicate urlEqualCondition = cb.equal(from.get(Url_.urlCode), filter.getUrlCode());
+			cq.where(urlEqualCondition);
+		}
+
+		// set sort params
+		if (filter.getSortProperty() != null) {
+			cq.orderBy(new OrderImpl(from.get(filter.getSortProperty()), filter.isSortOrder()));
+		}
+
+		TypedQuery<Url> q = em.createQuery(cq);
+
+		// set paging
+		if (filter.getOffset() != null && filter.getLimit() != null) {
+			q.setFirstResult(filter.getOffset());
+			q.setMaxResults(filter.getLimit());
+		}
+
+		// set execute query
+		List<Url> allitems = q.getResultList();
+		return allitems;
 	}
 
 	@Override
 	public Url find(String urlCode) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = getEntityManager();
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+
+		CriteriaQuery<Url> cq = cb.createQuery(Url.class);
+
+		Root<Url> from = cq.from(Url.class);
+
+		cq.select(from);
+		Predicate urlEqualCondition = cb.equal(from.get(Url_.urlCode), urlCode);
+		cq.where(urlEqualCondition);
+
+		TypedQuery<Url> q = em.createQuery(cq);
+
+		List<Url> allitems = q.getResultList();
+
+		if (allitems.isEmpty()) {
+			return null;
+		} else if (allitems.size() == 1) {
+			return allitems.get(0);
+		} else {
+			throw new IllegalArgumentException("more than 1 user found ");
+		}
 	}
 
 	@Override
 	public Url getWithTags(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = getEntityManager();
+
+		if (em.find(getEntityClass(), id) == null) {
+			return null;
+		};
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+
+		CriteriaQuery<Url> cq = cb.createQuery(Url.class);
+
+		Root<Url> from = cq.from(Url.class);
+
+		// set selection
+		cq.select(from);
+
+		from.fetch(Url_.tags, JoinType.LEFT);
+
+		cq.where(cb.equal(from.get(Url_.id), id));
+		cq.distinct(true);
+
+		TypedQuery<Url> q = em.createQuery(cq);
+
+		// set execute query
+		return q.getSingleResult();
 	}
 
 }
