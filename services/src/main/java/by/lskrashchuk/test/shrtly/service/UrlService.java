e2a2e21be2167f90494208b26f52ee063c6489e8
Larisa Skrashchuk <lskrashchuk@gmail.com>
2016-08-19 16:18:58 +0300

    Added UrlService

---
+++ services/src/main/java/by/lskrashchuk/test/shrtly/service/UrlService.java
@@ -0,0 +1,26 @@
+package by.lskrashchuk.test.shrtly.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import by.lskrashchuk.test.shrtly.dataaccess.filters.UrlFilter;
+import by.lskrashchuk.test.shrtly.datamodel.Url;
+
+public interface UrlService {
+
+	Url getUrl(Long id);
+	
+	@Transactional
+	void saveOrUpdate(Url url);
+	
+	@Transactional
+	void delete(Url url);
+	
+    List<Url> find(UrlFilter filter);
+
+    List<Url> getAll();
+    
+    Long count(UrlFilter filter);
+    
+}
