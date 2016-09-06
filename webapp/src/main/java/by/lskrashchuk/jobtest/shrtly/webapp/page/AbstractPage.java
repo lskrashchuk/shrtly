eafa3f3363bae1a2281e9a584513ddef68d2bec1
Larisa Skrashchuk <lskrashchuk@gmail.com>
2016-09-06 15:19:46 +0300

    Fixed sign out

--- webapp/src/main/java/by/lskrashchuk/jobtest/shrtly/webapp/page/AbstractPage.java
+++ webapp/src/main/java/by/lskrashchuk/jobtest/shrtly/webapp/page/AbstractPage.java
@@ -7,19 +7,18 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import by.lskrashchuk.jobtest.shrtly.webapp.app.AuthorizedSession;
 import by.lskrashchuk.jobtest.shrtly.webapp.page.home.HomePage;
 import by.lskrashchuk.jobtest.shrtly.webapp.page.login.LoginPage;
 import by.lskrashchuk.jobtest.shrtly.webapp.page.signup.SignUpPage;
 
-
-
-public abstract class AbstractPage extends WebPage implements Serializable{
+public abstract class AbstractPage extends WebPage implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	public AbstractPage() {
 		super();
 	}
@@ -27,74 +26,73 @@ public abstract class AbstractPage extends WebPage implements Serializable{
 	public AbstractPage(PageParameters parameters) {
 		super(parameters);
 	}
-	
+
 	@Override
 	protected void onInitialize() {
 		super.onInitialize();
-		
+
 		Link<Void> linkHome = new Link<Void>("link-home") {
-            /**
+			/**
 			 * 
 			 */
 			private static final long serialVersionUID = 1L;
 
 			@Override
-            public void onClick() {
-                setResponsePage(new HomePage());
-            }
+			public void onClick() {
+				setResponsePage(new HomePage());
+			}
 		};
 		add(linkHome);
-	       
+
 		Link<Void> linkLogin = new Link<Void>("link-login") {
-            /**
+			/**
 			 * 
 			 */
 			private static final long serialVersionUID = 1L;
 
 			@Override
-            public void onClick() {
-                setResponsePage(new LoginPage());
-            }
-        };
+			public void onClick() {
+				setResponsePage(new LoginPage());
+			}
+		};
 		add(linkLogin);
 
 		Link<Void> linkSignUp = new Link<Void>("link-signup") {
-            /**
+			/**
 			 * 
 			 */
 			private static final long serialVersionUID = 1L;
 
 			@Override
-            public void onClick() {
-                setResponsePage(new SignUpPage());
-            }
-        };
+			public void onClick() {
+				setResponsePage(new SignUpPage());
+			}
+		};
 		add(linkSignUp);
 
 		Link<Void> linkSignOut = new Link<Void>("link-signout") {
-            /**
+			/**
 			 * 
 			 */
 			private static final long serialVersionUID = 1L;
 
 			@Override
-            public void onClick() {
-                getSession().invalidate();
-                setResponsePage(new HomePage());
-            }
-        };
+			public void onClick() {
+				AuthorizedSession.get().invalidate();
+				setResponsePage(AuthorizedSession.get().getApplication().getHomePage());
+			}
+		};
 		add(linkSignOut);
-		
-        if (AuthenticatedWebSession.get().isSignedIn()) {
-        	linkLogin.setVisible(false);
-        	linkSignUp.setVisible(false);
-        	linkSignOut.setVisible(true);
-        }
-        else {
-        	linkLogin.setVisible(true);
-        	linkSignUp.setVisible(true);
-        	linkSignOut.setVisible(false);
-        }
+
+		if (AuthenticatedWebSession.get().isSignedIn()) {
+			linkLogin.setVisible(false);
+			linkSignUp.setVisible(false);
+			linkSignOut.setVisible(true);
+		} else {
+			linkLogin.setVisible(true);
+			linkSignUp.setVisible(true);
+			linkSignOut.setVisible(false);
+		}
 	}
-	
+
 }
